b501b911a1fe6b14d95de4d43901cbe86d08f279
Andrew Lunde <andrew.lunde@sap.com>
2018-10-08 16:03:37 -0400

    push

--- hxe_python_ml.sh
+++ hxe_python_ml.sh
@@ -24,6 +24,33 @@ echo ""
 echo "hxehost login: hxeadm"
 echo "Password: $hxeadmpw"
 
+#as root
+zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution --type pattern devel_basis
 
+zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution tk-devel tcl-devel libffi-devel openssl-devel readline-devel sqlite3-devel ncurses-devel xz-devel zlib-devel
 
+#as hxeadm
+
+wget http://thedrop.sap-a-team.com/files/hana_ml-1.0.3.tar.gz
+wget http://thedrop.sap-a-team.com/files/XS_PYTHON00_0-70003433.ZIP
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
+
+tar xzvf Python-3.6.5.tgz
+md python_3_6_5
+cd Python-3.6.5
+./configure --prefix=/usr/sap/HXE/HDB90/hxe_python_ml/python_3_6_5/ --exec-prefix=/usr/sap/HXE/HDB90/hxe_python_ml/python_3_6_5/ ; make -j4 ; make altinstall
+
+cd ../python_3_6_5/bin
+
+ln -s easy_install-3.6 easy_install
+ln -s pip3.6 pip
+ln -s pydoc3.6 pydoc
+ln -s python3.6 python
+ln -s pyvenv-3.6 pyvenv
+
+xs create-runtime -p /usr/sap/HXE/HDB90/hxe_python_ml/python_3_6_5/
+
+cd ../..
+
+unzip XS_PYTHON00_0-70003433.ZIP -d sap_dependencies
 
---
+++ set_python_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PYTHONHOME=/usr/sap/HXE/HDB90/hxe_python_ml/python_3_6_5
+unset PYTHONSTARTUP
+export PATH=$PYTHONHOME/bin:$PATH
+export PYTHONUSERBASE=/usr/sap/HXE/HDB90/hxe_python_ml/python/vendor
+#
+# For local building of NodeJS
+npm config set @sap:registry "https://npm.sap.com/" ; npm config set registry "https://registry.npmjs.org/" ; npm config set strict-ssl true
+#
+vcapsvcs=$(cat <<EOF
+{
+}
+EOF
+)
+export VCAP_SERVICES=$vcapsvcs
+
